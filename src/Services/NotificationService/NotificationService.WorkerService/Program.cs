45f07cad83eb6c119d1af3ca2724d1081e51f997
Yunus <yunusozdemir468@gmail.com>
2022-07-31 16:07:09 +0300

    Initial Create

---
+++ src/Services/NotificationService/NotificationService.WorkerService/Program.cs
@@ -0,0 +1,11 @@
+﻿using NotificationService.WorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
+
