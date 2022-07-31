85e4841b2293c1da6c2b59b059151a07147cb3cc
Yunus <yunusozdemir468@gmail.com>
2022-07-31 18:59:48 +0300

    EventBus.Base finished

--- src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBusSubscriptionManager.cs
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBusSubscriptionManager.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using EventBus.Base.Events;
+
 namespace EventBus.Base.Abstraction
 {
     public interface IEventBusSubscriptionManager
     {
         bool IsEmpty { get; }
         event EventHandler<string> OnEventRemoved;
-        void AddSubscription<T, TH>() where IntegrationEvent
-
+        void AddSubscription<T, TH>() where T: IntegrationEvent where TH : IIntegrationEventHandler<T>;
+        void RemoveSubscription<T, TH>() where TH : IIntegrationEventHandler<T> where T : IntegrationEvent;
+        bool HasSubscriptionsForEvent<T>() where T : IntegrationEvent;
+        bool HasSubscriptionsForEvent(string eventName);
+        Type GetEventTypeByName(string eventName);
+        void Clear();
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent;
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName);
+        string GetEventKey<T>();
     }
 }
 
---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/BaseEventBus.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using EventBus.Base.Abstraction;
+using EventBus.Base.SubManagers;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace EventBus.Base.Events
+{
+    public abstract class BaseEventBus:IEventBus
+    {
+        public readonly IServiceProvider ServiceProvider;
+
+        public readonly IEventBusSubscriptionManager SubManager;
+        public EventBusConfig EventBusConfig { get; private set; }
+
+        protected BaseEventBus(EventBusConfig config,IServiceProvider serviceProvider)
+        {
+            EventBusConfig = config;
+            ServiceProvider = serviceProvider;
+            SubManager = new InMemoryEventBusSubscriptionManager(ProcessEventName);
+        }
+
+        public virtual void Dispose()
+        {
+            EventBusConfig = null;
+            SubManager.Clear();
+        }
+
+        public virtual string ProcessEventName(string eventName)
+        {
+            if (EventBusConfig.DeleteEventPrefix)
+                eventName = eventName.TrimStart(EventBusConfig.EventNamePrefix.ToArray());
+            if (EventBusConfig.DeleteEventSuffix)
+                eventName = eventName.TrimStart(EventBusConfig.EventNameSuffix.ToArray());
+
+            return eventName;
+        }
+
+        public virtual string GetSubName(string eventName)
+        {
+            return $"{EventBusConfig.SubscriberClientAppName}.{ProcessEventName(eventName)}";
+        }
+        public async Task<bool> ProcessEvent(string eventName,string message)
+        {
+            eventName = ProcessEventName(eventName);
+            var processed = false;
+            if (SubManager.HasSubscriptionsForEvent(eventName))
+            {
+                var subscriptions = SubManager.GetHandlersForEvent(eventName);
+                using (var scope = ServiceProvider.CreateScope())
+                {
+                    foreach (var subscription in subscriptions)
+                    {
+                        var handler = ServiceProvider.GetService(subscription.HandlerType);
+                        if (handler == null) continue;
+                        var eventType = SubManager.GetEventTypeByName($"{EventBusConfig.EventNamePrefix}{eventName}{EventBusConfig.EventNameSuffix}");
+                        var integrationEvent = JsonConvert.DeserializeObject(message, eventType);
+
+
+                        var concreteType = typeof(IIntegrationEventHandler<>).MakeGenericType(eventType);
+                        await (Task)concreteType.GetMethod("Handle").Invoke(handler, new object[] { integrationEvent });
+                    }
+                }
+                processed = true;
+            }
+            return processed;
+        }
+
+        public abstract void Publish(IntegrationEvent @event);
+
+
+        public abstract void Subscription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+
+        public abstract void UnSubscription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+    }
+}
+
